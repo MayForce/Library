ed4f8cc8d934b0d611d0ccee1e93e4516b431b58
MayForce <35632390+MayForce@users.noreply.github.com>
2018-03-05 19:57:48 +0300

    Delete Audiovisual.java

--- Audiovisual.java
+++
@@ -1,6 +0,0 @@
-public class Audiovisual extends Item {
-
-    public Audiovisual(String author, String title, String keywords, double price) {
-        super(author, title, keywords, price);
-    }
-}
